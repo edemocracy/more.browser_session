52e5c771814d7a0a78fa4f8e921e3b66fe5748c3
Tobias dpausp <dpausp@posteo.de>
2022-05-17 21:07:44 +0000

    Make Nix shell usable
    Now uses Niv to pin the nixpkgs version. We use nixos-unstable.

--- shell.nix
+++ shell.nix
@@ -1,23 +1,23 @@
 let
-  pkgs = import <nixpkgs> {};
+  sources = import nix/sources.nix {};
+  pkgs = import sources.nixpkgs {};
   lib = pkgs.lib;
   envVars = ''
     export GIT_SSL_CAINFO="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
   '';
 
-  pythonTest = pkgs.python39.withPackages (ps: with ps; [ pytest ]);
-
 in pkgs.mkShell {
   name = "more-browser-session";
   buildInputs = with pkgs; [
-    cacert
     entr
-    zsh
+    niv
   ] ++
-  (with python39Packages; [
+  (with pkgs.python310Packages; [
+    black
     build
+    isort
+    interpreter
     pylint
-    pythonTest
     setuptools_scm
     twine
     wheel
